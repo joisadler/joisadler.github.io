3bc45616362c73f3fbf19c689c939ad94630707b
Yossi Adler <yossi@startupbooster.dev>
2021-08-11 23:48:15 +0300

    update

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem "jekyll-sitemap"
 gem "jekyll-gist"
 gem 'jekyll-mentions'
 gem 'jekyll-feed'
+
+gem "webrick", "~> 1.7"
---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi-universal-darwin20/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/yossi/Projects/joisadler.github.io/vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/arm/ffitarget.h
