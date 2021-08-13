dce81cddefdd56c5ae416ed8fc30f35feb4d58e8
Yossi Adler <yossi@startupbooster.dev>
2021-08-13 23:46:35 +0300

    update

--- vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi-universal-darwin20/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/yossi/Projects/joisadler.github.io/vendor/bundle/ruby/2.6.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/arm/ffitarget.h
--- vendor/bundle/ruby/2.6.0/gems/html-pipeline-2.9.1/lib/html/pipeline/version.rb
+++
@@ -1,5 +0,0 @@
-module HTML
-  class Pipeline
-    VERSION = '2.9.1'.freeze
-  end
-end
